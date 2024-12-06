f5f7a9ea5d9be8d9811a4614b5a723b32c75342d
Ardit Zubaku <zubaku92@gmail.com>
2024-12-06 20:38:44 +0100

    feat: Constants class for better book-keeping.

---
+++ app/src/main/java/com/zubaku/memoir/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.zubaku.memoir.utils;
+
+public class Constants {
+  public static final String POST_ID = "POST_ID";
+  public static final String TITLE = "TITLE";
+  public static final String DESCRIPTION = "DESCRIPTION";
+  public static final String IMAGE_URL = "IMAGE_URL";
+  public static final String YES = "Yes";
+  public static final String NO = "No";
+}
