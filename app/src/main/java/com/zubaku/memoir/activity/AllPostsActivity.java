fef4e01340589dfd39d2c4d6cdb36de3eed73710
Ardit Zubaku <zubaku92@gmail.com>
2024-12-06 01:39:31 +0100

    feat: Add a menu - override onCreateOptionsMenu.

--- app/src/main/java/com/zubaku/memoir/activity/AllPostsActivity.java
+++ app/src/main/java/com/zubaku/memoir/activity/AllPostsActivity.java
@@ -1,15 +1,41 @@
 package com.zubaku.memoir.activity;
 
 import android.os.Bundle;
-
+import android.view.Menu;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.zubaku.memoir.R;
+import com.zubaku.memoir.adapter.AllPostsAdapter;
+import com.zubaku.memoir.model.Post;
+import com.zubaku.memoir.utils.Collections;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AllPostsActivity extends AppCompatActivity {
+  private FirebaseAuth auth;
+  private FirebaseUser currentUser;
+
+  // Firebase Firestore
+  private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+  private final CollectionReference collectionReference = db.collection(Collections.Posts);
+
+  // List of Journals
+  private List<Post> postsList;
+
+  // RecyclerView
+  private RecyclerView recyclerView;
+
+  // Adapter
+  private AllPostsAdapter allPostsAdapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +49,23 @@ public class AllPostsActivity extends AppCompatActivity {
           v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
           return insets;
         });
+
+    auth = FirebaseAuth.getInstance();
+    currentUser = auth.getCurrentUser();
+
+    // Widgets
+    recyclerView = findViewById(R.id.recyclerView);
+    recyclerView.setHasFixedSize(true);
+    recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    postsList = new ArrayList<>();
+  }
+
+  // Add a menu
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.post_menu, menu);
+    return super.onCreateOptionsMenu(menu);
   }
 }
